2bc22d19f7c51586f74829e6b2ec91897bf32251
mr.vjcspy <mr.vjcspy@gmail.com>
2016-06-21 22:51:03 +0700

    - create post table

---
+++ Database/Migrations/2016_06_21_150917_create_izblog_post_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIzblogPostTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create(
+            'izblog_post',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('post_author');
+                $table->dateTime('post_date');
+                $table->longText('post_content');
+                $table->text('post_title');
+                $table->char('post_status', 20);
+                $table->char('ping_status', 20);
+                $table->char('post_password');
+                $table->char('post_name');
+                $table->char('post_type', 20);
+                $table->integer('post_parent_id');
+                $table->integer('comment_count');
+                $table->timestamps();
+                $table->index('id');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('izblog_post');
+    }
+
+}
---
+++ Entities/Post.php
@@ -0,0 +1,9 @@
+<?php namespace Modules\Izblog\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model {
+
+    protected $fillable = [];
+
+}
